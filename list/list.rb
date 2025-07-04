c8e37064be249a8ee42ec10fbc718381e0605c36
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2025-07-04 09:38:01 -0500

    Complete dynamic directory index project

--- list/list.rb
+++ list/list.rb
@@ -1,9 +1,11 @@
 require 'tilt/erubi'
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader'
 
-get "/" do
-  @contents = Dir.glob("public/*")
+get '/' do
+  @files = Dir.glob('*', base: 'public')
+  @files.sort_by!(&:downcase) if params[:sort] == 'asc'
+  @files.sort_by!(&:downcase).reverse! if params[:sort] == 'desc'
 
   erb :list
 end
