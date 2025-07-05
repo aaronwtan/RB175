19cfbd8963c260a21e18516c814d8cbcb8a30dcf
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2025-07-05 11:32:48 -0500

    Complete Users and Interests coding challenge

--- lesson_3/users_and_interests/users_and_interests.rb
+++ lesson_3/users_and_interests/users_and_interests.rb
@@ -1,7 +1,40 @@
+require 'yaml'
+
 require 'tilt/erubi'
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader'
+
+before do
+  @users = YAML.load_file('users.yaml')
+end
 
 get "/" do
+  redirect "/users"
+end
 
+get "/users" do
+  @title = 'Users and Interests'
+
+  erb :users
+end
+
+get "/users/:name" do
+  @name, @email, @interests, @other_users = parse_users
+
+  erb :user
+end
+
+helpers do
+  def count_interests
+    @users.values.reduce(0) { |total, info| total + info[:interests].size }
+  end
+end
+
+def parse_users
+  name = params[:name].to_sym
+  email = @users[name][:email]
+  interests = @users[name][:interests]
+  other_users = @users.keys.reject { |user| user == name }
+
+  [name, email, interests, other_users]
 end
